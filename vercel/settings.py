fb6a986864d300ebc6aee87add2f160def3437b4
dhimanparas20 <dhimanparas20@gmail.com>
2024-11-29 15:34:37 +0530

    better congis
    Signed-off-by: dhimanparas20 <dhimanparas20@gmail.com>

--- vercel/settings.py
+++ vercel/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x_8*knkz%kn1c)w$5nj%u*r3#5f887k^7xp%=6gt#$gq-r25s^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
